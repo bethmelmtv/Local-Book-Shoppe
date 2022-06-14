57d5244a9438c17fc3e8f3c4a5b3e001bd0c9181
Beth <bethmelmtv@gmail.com>
2022-06-14 15:00:56 -0700

    worked on join table

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,6 +2,7 @@
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP table if exists books;
 DROP table if exists authors;
+DROP table if exists books_authors;
 
 CREATE table books (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -24,3 +25,17 @@ INSERT into authors (name) VALUES
 ('Ernest Hemmingway'),
 ('Ernest Play'),
 ('James Patterson');
+
+
+CREATE table books_authors(
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  author_id BIGINT,
+  book_id BIGINT,
+  FOREIGN KEY(author_id) REFERENCES authors(id),
+  FOREIGN KEY(book_id) REFERENCES books(id)
+);
+  
+INSERT INTO books_authors(author_id,book_id) VALUES
+(1,1),
+(2,2),
+(3,3);
