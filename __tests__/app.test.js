caa56a88a12f1e29a5dd268b679c0f479b74da80
Beth <bethmelmtv@gmail.com>
2022-06-14 12:45:43 -0700

    get All books function working and passing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,21 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const { books } = require('../data/books');
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/books should return a list of all books', async () => {
+    const resp = await request(app).get('/books');
+    const expected = books.map((book) => {
+      return { id: book.id, title: book.title, released: book.released };
+    });
+    expect(resp.body).toEqual(expected);
   });
+
   afterAll(() => {
     pool.end();
   });
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,14 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists books;
+
+CREATE table books (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR NOT NULL,
+  released INT NOT NULL
+);
+
+INSERT into books (title, released) VALUES
+('Winnie the Pooh', 1984 ),
+('Clifford the Big Red Dog', 1940),
+('The Giving Tree', 1924);
